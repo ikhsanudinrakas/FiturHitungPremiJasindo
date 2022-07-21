d79071be5dbe33d4fbb7dc72d27bdd215254dd60
Kevin Tanuwijaya <kevin.119140207@student.itera.ac.id>
2022-07-21 08:00:06 +0700

    m
    m

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-<script>
-    var 
-</script>
+function nilai_input() {
+
+    var batas_atas = 0.05;
+    var batas_bawah = 0.02;
+
+    // AMBIK NILAI DARI FORM INPUT
+    var nilai = document.getElementById('nilai').value;
+
+    // HASIL
+    var nilai_atas = nilai*batas_atas;
+    var nilai_bawah = nilai*batas_bawah;
+
+    // TAMPIL HASIL
+    document.getElementById("hasil").innerHTML= "Rp. "+   nilai_bawah.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")+ "  -  Rp. "  +nilai_atas.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    
+}
+    
+
