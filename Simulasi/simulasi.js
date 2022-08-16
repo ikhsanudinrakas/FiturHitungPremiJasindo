7c0014554f540a52db66fb45b94b82378bfea337
Kevin Tanuwijaya <kevin.119140207@student.itera.ac.id>
2022-08-16 14:21:00 +0700

    Update simulasi.js

--- Simulasi/simulasi.js
+++ Simulasi/simulasi.js
@@ -1,35 +1,3 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
-
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 1024,
-    height: 720,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
-
-  win.loadFile('simulasi.html')
-}
-
-app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-// ELECTRON END
-
 function remove_dot(x)
 {
     return x.split(/[.,]/).join("");
